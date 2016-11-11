7cfdcd688a6cefb7579aa11f282341ccaf38a182
Prashanth <janagama.prashanth@gmail.com>
2016-11-10 23:40:50 -0500

    6 Routing code

---
+++ app/heroes.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+import { OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-heroes',
+    templateUrl: 'heroes.component.html',
+    styleUrls: ['heroes.component.css']
+
+})
+export class HeroesComponent implements OnInit { 
+ title = 'Tour of Heroes';
+ heroes: Hero[];;
+ selectedHero: Hero;
+ 
+ constructor(
+   private heroService: HeroService,
+   private router: Router
+           ){}
+ 
+ gotoDetail(): void {
+   this.router.navigate(['/detail', this.selectedHero.id]);
+ }
+
+ getHeroes(): void{
+ 	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+ }
+
+ onSelect(hero: Hero): void{
+ 	this.selectedHero = hero;
+ }
+
+ ngOnInit(): void{
+ 	this.getHeroes();
+ }
+
+}
+
+
